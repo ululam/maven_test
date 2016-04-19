2f1f6a46ca23bcd4c9245de9c0a017231bd797ff
AlexeyZ <alexey.zakharchenko@gteamstaff.com>
2016-04-19 14:19:06 +0300

    Travis test on after success

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,6 +15,8 @@ public class App {
     }
 
     public static void main(String args[]) {
+        System.out.println("****************************************");
         System.out.println("hello world");
+        System.out.println("****************************************");
     }
 }
